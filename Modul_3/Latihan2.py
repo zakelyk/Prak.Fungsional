6bb78db70c4f867c2545ded2df1ece5feef75be7
zakelyk <candra.setyo.w@gmail.com>
2023-10-29 23:42:58 +0700

    Update Latihan2.py

--- Modul_3/Latihan2.py
+++ Modul_3/Latihan2.py
@@ -3,7 +3,7 @@ data = ["3 minggu 3 hari 7 jam 21 menit",
         "7 minggu 1 hari 5 jam 33 menit"]
 
 def filter_numbers(entry):
-    return [word for word in entry.split() if word.isdigit()]
+    return list(filter(lambda x: x.isdigit(), entry.split()))
 
 for entry in data:
     print(filter_numbers(entry))
