e630320d2412111f96a87b09a5d581e4e16cce82
zakelyk <candra.setyo.w@gmail.com>
2023-11-01 00:54:20 +0700

    Create SoalB.py

---
+++ Modul_3/SoalB.py
@@ -0,0 +1,14 @@
+movies = [
+    {"title": "Avengers: Endgame", "year": 2019, "rating": 8.4, "genre": "Action"},
+    {"title": "Parasite", "year": 2019, "rating": 8.6, "genre": "Drama"},
+    {"title": "Nomadland", "year": 2020, "rating": 7.3, "genre": "Drama"},
+    {"title": "Dune", "year": 2021, "rating": 7.9, "genre": "Sci-Fi"},
+    {"title": "Spider-Man: No Way Home", "year": 2021, "rating": 7.6, "genre": "Action"},
+    {"title": "The French Dispatch", "year": 2021, "rating": 7.0, "genre": "Comedy"},
+    {"title": "A Quiet Place Part II", "year": 2020, "rating": 7.4, "genre": "Horror"},
+    {"title": "No Time to Die", "year": 2021, "rating": 6.8, "genre": "Action"},
+    {"title": "The Power of the Dog", "year": 2021, "rating": 7.3, "genre": "Drama"},
+    {"title": "Eternals", "year": 2021, "rating": 6.4, "genre": "Action"},
+    {"title": "The Last Duel", "year": 2021, "rating": 7.0, "genre": "Drama"},
+]
+
